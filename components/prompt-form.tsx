d5ae524cbbb9d1564ebef8082a808a76ed883e68
Beenish Khan <beenishkhan603@gmail.com>
2024-08-21 16:53:26 +0500

    add: supabase auth

--- components/prompt-form.tsx
+++ components/prompt-form.tsx
@@ -5,7 +5,7 @@ import Textarea from 'react-textarea-autosize'
 
 import { useActions, useUIState } from 'ai/rsc'
 
-import { UserMessage } from './stocks/message'
+import { BotMessage, UserMessage } from './stocks/message'
 import { type AI } from '@/lib/chat/actions'
 import { Button } from '@/components/ui/button'
 import { IconArrowElbow, IconPlus } from '@/components/ui/icons'
@@ -17,7 +17,7 @@ import {
 import { useEnterSubmit } from '@/lib/hooks/use-enter-submit'
 import { nanoid } from 'nanoid'
 import { useRouter } from 'next/navigation'
-
+import axios from 'axios'
 export function PromptForm({
   input,
   setInput
@@ -62,8 +62,40 @@ export function PromptForm({
         ])
 
         // Submit and get response message
-        const responseMessage = await submitUserMessage(value)
-        setMessages(currentMessages => [...currentMessages, responseMessage])
+        // const responseMessage = await submitUserMessage(value)
+        // setMessages(currentMessages => [...currentMessages, responseMessage])
+        //  console.log('response Message', responseMessage)
+        let data = JSON.stringify({
+          project_id: '2',
+          text: 'who am I?'
+        })
+        let config = {
+          method: 'post',
+          maxBodyLength: Infinity,
+          url: 'http://localhost:8000/query_data',
+          headers: {
+            'access-token':
+              'eyJhbGciOiJIUzI1NiIsImtpZCI6IkdQR3ZRL3p0bE11K25KK1QiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3pxcmhxaGJ5YnZiYnlubXdsZHJ4LnN1cGFiYXNlLmNvL2F1dGgvdjEiLCJzdWIiOiJjMDkzNDg5MC0yOWMzLTQxNTktODRkZC1iNmZjZmRlM2M0ODMiLCJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzI0MjQyMjk3LCJpYXQiOjE3MjQyMzg2OTcsImVtYWlsIjoiYm9uZXRpODUxNEBmdXppdGVhLmNvbSIsInBob25lIjoiIiwiYXBwX21ldGFkYXRhIjp7InByb3ZpZGVyIjoiZW1haWwiLCJwcm92aWRlcnMiOlsiZW1haWwiXX0sInVzZXJfbWV0YWRhdGEiOnsiZW1haWwiOiJib25ldGk4NTE0QGZ1eml0ZWEuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJwaG9uZV92ZXJpZmllZCI6ZmFsc2UsInN1YiI6ImMwOTM0ODkwLTI5YzMtNDE1OS04NGRkLWI2ZmNmZGUzYzQ4MyJ9LCJyb2xlIjoiYXV0aGVudGljYXRlZCIsImFhbCI6ImFhbDEiLCJhbXIiOlt7Im1ldGhvZCI6InBhc3N3b3JkIiwidGltZXN0YW1wIjoxNzI0MjMyOTQ2fV0sInNlc3Npb25faWQiOiJjMDRlNDY2Ny1hYjNiLTQ1MzYtODU2ZS1mY2EwYTI5MGNjZjciLCJpc19hbm9ueW1vdXMiOmZhbHNlfQ.vNpdjkS5Nn2bkj--noKX6tvHpmD2EiDS1wZFGpzeJEM',
+            'Content-Type': 'application/json'
+          },
+          data: data
+        }
+
+        axios
+          .request(config)
+          .then(response => {
+            console.log(JSON.stringify(response.data))
+            setMessages(currentMessages => [
+              ...currentMessages,
+              {
+                id: nanoid(), // Generate a unique ID for the message
+                display: <BotMessage content={response.data.results} /> // Display the data (e.g., as preformatted text)
+              }
+            ])
+          })
+          .catch(error => {
+            console.log(error)
+          })
       }}
     >
       <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background px-8 sm:rounded-md sm:border sm:px-12">
